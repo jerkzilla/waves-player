aa06885959a46e540984f1e5ffa034c0dbcf484d
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-04 17:21:06 -0600

    got song to start playing and pause on click

--- src/App.js
+++ src/App.js
@@ -7,10 +7,15 @@ import data from "./util";
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
