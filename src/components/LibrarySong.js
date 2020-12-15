58033193a21f2aff4ee32ff2ec800fabb779e49e
jerkzilla <johnjamesmarshalljr@gmail.com>
2020-12-15 11:07:26 -0500

    intro async and await and deleted util file

--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { playAudio } from "../util";
 const LibrarySong = ({
   song,
   songs,
@@ -9,8 +8,8 @@ const LibrarySong = ({
   isPlaying,
   setSongs,
 }) => {
-  const songSelectHandler = () => {
-    setCurrentSong(song);
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
     const newSongs = songs.map((song) => {
       if (song.id === id) {
         return {
@@ -26,7 +25,7 @@ const LibrarySong = ({
     });
 
     setSongs(newSongs);
-    playAudio(isPlaying, audioRef);
+    if (isPlaying) audioRef.current.play();
   };
 
   return (
